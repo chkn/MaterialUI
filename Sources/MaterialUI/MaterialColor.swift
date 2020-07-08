71a25a9229359aba10d37ce8a42fa565c6e47b4d
ConfusedVorlon <rob@hobbyistsoftware.com>
2020-07-08 04:18:09 +0100

    Move accent to protocol (#3)
    Fixes: Provide a way to set custom accent colour #2

---
+++ Sources/MaterialUI/MaterialColor.swift
@@ -0,0 +1,35 @@
+//
+//  MaterialColor.swift
+//  MaterialUIMac
+//
+//  Created by Rob Jonson on 11/07/2020.
+//  Copyright © 2020 HobbyistSoftware. All rights reserved.
+//
+
+import SwiftUI
+
+
+//introduce custom environment key to allow setting accent colour in MacOS
+//https://medium.com/@SergDort/custom-environment-keys-in-swiftui-49f54a13d140
+struct MaterialAccentColorKey: EnvironmentKey {
+    static let defaultValue: Color = Color.accentColor
+}
+
+extension EnvironmentValues {
+    
+    /// Use to set accent colour in MacOS
+    /// e.g.
+    ///
+    ///    Button("Outlined Button", action: {})
+    ///    .buttonStyle(OutlinedButtonStyle())
+    ///    .environment(\.materialAccent, Color.green)
+    var materialAccent: Color {
+        get {
+            let color = self[MaterialAccentColorKey.self]
+            return color
+        }
+        set {
+            self[MaterialAccentColorKey.self] = newValue
+        }
+    }
+}
